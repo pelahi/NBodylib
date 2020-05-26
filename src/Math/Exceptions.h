f10f27cf089a8e53ed586ccc934630ae069bdd20
Pascal <pascaljelahi@gmail.com>
2020-05-26 15:38:23 +0800

    update to gsl error handling

--- src/Math/Exceptions.h
+++ src/Math/Exceptions.h
@@ -54,13 +54,8 @@ public:
 
 };
 
-void throw_exception_gsl_handler(const char *reason, const char *file, int line, int gsl_errno){
-    throw Math::gsl_error(reason, file, line, gsl_errno, gsl_strerror(gsl_errno));
-}
-void install_gsl_error_handler() {
-    gsl_set_error_handler(&throw_exception_gsl_handler);
-}
-
+void throw_exception_gsl_handler(const char *reason, const char *file, int line, int gsl_errno);
+void install_gsl_error_handler(); 
 }
 
 
